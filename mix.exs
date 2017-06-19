5bf7ae2c2bcf33a4d6b1dcd046c525ddec2687cf
Elliott Davis <edavis@chef.io>
2017-06-19 11:45:19 -0500

    prepare for publishing to hex

--- mix.exs
+++ mix.exs
@@ -2,31 +2,41 @@ defmodule Steroids.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :steroids,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :steroids,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Steroids",
+      source_url: "https://github.com/elliott-davis/steroids"
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  def description do
+    """
+    Steroids is an ElasticSearch query body builder adapted from [Bodybuilder](https://github.com/danpaz/bodybuilder).
+    Easily build complex queries for elasticsearch with a simple, predictable api.
+    """
+  end
+
+  def package do
+    [
+      name: :steroids,
+      files: ["lib", "mix.exs", "LICENSE*", "README*"],
+      licenses: ["Apache 2.0"],
+      maintainers: ["Elliott Davis"],
+      links: %{ "GitHub" => "https://github.com/elliott-davis/steroids" }
+    ]
+  end
+
   defp deps do
     [
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
